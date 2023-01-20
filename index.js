09f0ed76d11ff497d83a786750f8aa2cf38649dc
SamSvetlik <sam.svetlik@gmail.com>
2023-01-19 20:53:45 -0600

    2nd commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hi!")
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello!"
+    })
+})
+
+app.listen(PORT, ()=> console.log(`I am listening on port ${PORT}`))
