c665c406e56067b61bfc1224e84ca9c5f72fa36c
SamSvetlik <sam.svetlik@gmail.com>
2023-01-20 20:04:29 -0600

    chore: DB setup, some CRUD, secrets hidden

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const express = require('express')
 const app = express()
+const pool = require('./sql/connection')
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Hello!"
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    pool.query("SELECT * FROM users", (err, rows, fields)=> {
+        res.json(rows)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params
+    pool.query(`SELECT * FROM users WHERE id = ${id}`, (err, rows, fields)=> {
+        res.json(rows)
+    })
+})
+
+app.post('/users', (req, res) => {
+    const { name, email, password } = req.body
+    pool.query('INSERT INTO users (id, name, email, password) VALUES (?, ?, ?, ?)',
+    [null, name, email, password],
+    (err, results, fields) => {
+        res.json(results)
+    })
+})
+
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params
+    pool.query(`UPDATE users SET ? WHERE id = ?`,
+    [req.body, id],
+    (err, results, fields) => {
+        res.json(results)
+    })
+})
+
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params
+    pool.query(`DELETE FROM users WHERE id = ?`,
+    [id],
+    (err, results, fields) => {
+        res.json(results)
     })
 })
 
