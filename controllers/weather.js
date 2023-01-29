35bbd5e1612d281ca520326e7d24e241b548f96b
SamSvetlik <sam.svetlik@gmail.com>
2023-01-29 12:41:46 -0600

    chore: added weather route (complete)

--- controllers/weather.js
+++ controllers/weather.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 const getWeather = async (req, res) => {
     const { input } = req.body
-    axios.get(`http://api.weatherapi.com/v1/forecast.json?key44a53e573f684b4784430731223012&q=${input}&days=2`)
-        .then(response => res.json(response))
+    axios.get(`http://api.weatherapi.com/v1/forecast.json?key=44a53e573f684b4784430731223012&q=${input}&days=2`)
+        .then(response => res.json(response.data))
 }
 
 module.exports = {getWeather}
